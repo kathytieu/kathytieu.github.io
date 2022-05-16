9ed9783cd35fafbe99535d1b7d3c584fa7328437
kathytieu <tieukathy@gmail.com>
2022-05-15 17:24:08 -0700

    scnrTestAnimation

---
+++ scanner/Lottie Test/index.js
@@ -0,0 +1,31 @@
+var animation = lottie.loadAnimation({
+    container: document.getElementById('10xfaster'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '10xfaster.json'
+})
+
+var animation = lottie.loadAnimation({
+    container: document.getElementById('serverless'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'serverless.json'
+})
+
+var animation = lottie.loadAnimation({
+    container: document.getElementById('scale'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'scale.json'
+})
+
+var animation = lottie.loadAnimation({
+    container: document.getElementById('integration'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'integration.json'
+})
