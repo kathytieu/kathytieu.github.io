7088db686402206b38d1e2d6f4f30b192240f860
kathytieu <tieukathy@gmail.com>
2022-09-19 14:41:01 -0700

    marketing site wip

---
+++ scanner/marketing_sep22/index.js
@@ -0,0 +1,31 @@
+var animation = lottie.loadAnimation({
+    container: document.getElementById('fast'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'fast.json'
+})
+
+var animation = lottie.loadAnimation({
+    container: document.getElementById('serverless'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'serverless.json'
+})
+
+var animation = lottie.loadAnimation({
+    container: document.getElementById('backlog'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'backlog.json'
+})
+
+var animation = lottie.loadAnimation({
+    container: document.getElementById('integrate'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'integrate.json'
+})
