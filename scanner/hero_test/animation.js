cc4312d86159ebecd01a3dded23e4ea3b02cce6f
kathytieu <tieukathy@gmail.com>
2024-07-02 07:33:25 -0700

    timepicker test for the wolves

---
+++ scanner/hero_test/animation.js
@@ -0,0 +1,7 @@
+var animation = lottie.loadAnimation({
+    container: document.getElementById('timepicker'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'timepicker.json'
+})
